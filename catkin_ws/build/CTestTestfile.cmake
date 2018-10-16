b5afe2b710bab648a81c42c79f64f449bc28aa1c
NozomuOhashi <ohashi@nagoya-u.jp>
2018-10-16 16:50:24 +0900

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ohashi/localization_kitti_ws/catkin_ws/src
+# Build directory: /home/ohashi/localization_kitti_ws/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
