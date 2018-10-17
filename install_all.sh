c60130cb6de292a692e08678eb69e46fd52a808f
NozomuOhashi <ohashi@nagoya-u.jp>
2018-10-17 16:01:34 +0900

    add limo

---
+++ install_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+sudo apt-get update
+
+echo "/-----　　 Install Autoware　　　-----/"
+sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-catkin libmosquitto-dev gksu
+cd Autoware/ros/src
+catkin_init_workspace
+cd ../
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+./catkin_make_release
+
+cd ../
+
+cd withous_catkin/
+sh install_limo.sh
+sh install_orbslam.sh
---
+++ withous_catkin/limo/src/ceres-bin/examples/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ohashi/localization_kitti_ws/withous_catkin/limo/src/ceres-solver-1.14.0/examples
+# Build directory: /home/ohashi/localization_kitti_ws/withous_catkin/limo/src/ceres-bin/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(sampled_function)
+subdirs(slam)
