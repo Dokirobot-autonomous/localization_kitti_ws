d178f13130a51e9dfc4c66afdb2eaa06222143c1
NozomuOhashi <ohashi@nagoya-u.jp>
2018-10-30 07:42:15 +0900

    delete submodules

--- catkin_ws/src/install_sptam.sh
+++ catkin_ws/src/install_sptam.sh
@@ -14,9 +14,11 @@ echo "\\/* Install ros-utils */\\"
 mkdir lib packages
 cd lib
 #git submodule add git@github.com:lrse/ros-utils.git
+git clone git@github.com:lrse/ros-utils.git
 
 echo "\\/* Install g2o */\\"
 #git submodule add git@github.com:RainerKuemmerle/g2o.git
+git clone git@github.com:RainerKuemmerle/g2o.git
 cd g2o
 git checkout 4b9c2f5b68d14ad479457b18c5a2a0bce1541a90
 mkdir build && cd build
@@ -28,8 +30,10 @@ cd ../..
 
 echo "\\/* Install DLib */\\"
 #git submodule add git@github.com:dorian3d/DLib.git
+git clone git@github.com:dorian3d/DLib.git
 cd DLib
-git checkout 70089a38056e8aebd5a2ebacbcb67d3751433f32
+
+git checkout -b ohashi_devel 70089a38056e8aebd5a2ebacbcb67d3751433f32
 mkdir build && cd build
 cmake ..
 cmake --build . --config Release
@@ -39,8 +43,9 @@ cd ../..
 
 echo "\\/* Install DBow2 */\\"
 #git submodule add git@github.com:dorian3d/DBoW2.git
-cd DBow2
-git checkout 82401cad2cfe7aa28ee6f6afb01ce3ffa0f59b44
+git clone git@github.com:dorian3d/DBoW2.git
+cd DBoW2
+git checkout -b ohashi_devel 82401cad2cfe7aa28ee6f6afb01ce3ffa0f59b44
 mkdir build && cd build
 cmake ..
 make
@@ -49,8 +54,9 @@ cd ../..
 
 echo "\\/* Install DLoopDetector */\\"
 #git submodule add git@github.com:dorian3d/DLoopDetector.git
+git clone git@github.com:dorian3d/DLoopDetector.git
 cd DLoopDetector
-git checkout 8e62f8ae84d583d9ab67796f779272b0850571ce
+git checkout -b ohashi_devel 8e62f8ae84d583d9ab67796f779272b0850571ce
 mkdir build && cd build
 cmake ..
 make
@@ -59,8 +65,9 @@ cd ../..
 
 echo "\\/* Install OpenGV */\\"
 #git submodule add git@github.com:laurentkneip/opengv.git
+git clone git@github.com:laurentkneip/opengv.git
 cd opengv
-git checkout 2e2d21917fd2fb75f2134e6d5be7a2536cbc7eb1
+git checkout -b ohashi_devel 2e2d21917fd2fb75f2134e6d5be7a2536cbc7eb1
 mkdir build && cd build
 cmake ..
 make
